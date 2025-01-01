6f869a034ddbbaecefa2bcfe7a6ee554a58d32df
Ocean <15811605102@163.com>
2025-01-01 19:05:02 +0800

    十六进制颜色平均分

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,112 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div v-for="(item,index) in cards" :key="index" class="card" :style="{'backgroundColor':item}"></div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  data() {
+    return {
+      cards: []
+    }
+  },
+  created() {
+    this.cards = this.gradientColor('#FF0000', '#00FF00', 20);
+  },
+  methods: {
+    gradientColor(startColor, endColor, step) {
+        var startRGB, startR,startG,startB
+        startRGB = this.colorRgb(startColor);//转换为rgb数组模式
+        startR = startRGB[0];
+        startG = startRGB[1];
+        startB = startRGB[2];
 
-nav {
-  padding: 30px;
+        var endRGB, endR,endG,endB
+        endRGB = this.colorRgb(endColor);
+        endR = endRGB[0];
+        endG = endRGB[1];
+        endB = endRGB[2];
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+        var sR, sG, sB;
+        sR = (endR - startR) / step;//总差值
+        sG = (endG - startG) / step;
+        sB = (endB - startB) / step;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        var colorArr = [];
+        for (var i = 0; i < step; i++) {
+            //计算每一步的hex值 
+            var hex = this.colorHex('rgb('+ parseInt((sR * i + startR))+ ',' + parseInt((sG * i + startG))+ ',' + parseInt((sB * i + startB)) + ')');
+            colorArr.push(hex);
+        }
+        return colorArr;
+    },
+    colorRgb(sColor){
+        var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+        var sColor = sColor.toLowerCase();
+        if (sColor && reg.test(sColor)) {
+            if (sColor.length === 4) {
+                var sColorNew = "#";
+                for (var i = 1; i < 4; i += 1) {
+                    sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1));
+                }
+                sColor = sColorNew;
+            }
+            //处理六位的颜色值
+            var sColorChange = [];
+            for (var i = 1; i < 7; i += 2) {
+                sColorChange.push(parseInt("0x" + sColor.slice(i, i + 2)));
+            }
+            return sColorChange;
+        } else {
+            return sColor;
+        }
+    },
+    colorHex(rgb){
+        var _this = rgb;
+        var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+        if (/^(rgb|RGB)/.test(_this)) {
+            var aColor = _this.replace(/(?:(|)|rgb|RGB)*/g, "").split(",");
+            var strHex = "#";
+            for (var i = 0; i < aColor.length; i++) {
+                var hex = Number(aColor[i]).toString(16);
+                hex = hex < 10 ? 0 + '' + hex : hex;// 保证每个rgb的值为2位
+                if (hex === "0") {
+                    hex += hex;
+                }
+                strHex += hex;
+            }
+            if (strHex.length !== 7) {
+                strHex = _this;
+            }
+            return strHex;
+        } else if (reg.test(_this)) {
+            var aNum = _this.replace(/#/, "").split("");
+            if (aNum.length === 6) {
+                return _this;
+            } else if (aNum.length === 3) {
+                var numHex = "#";
+                for (var i = 0; i < aNum.length; i += 1) {
+                    numHex += (aNum[i] + aNum[i]);
+                }
+                return numHex;
+            }
+        } else {
+            return _this;
+        }
     }
   }
+}
+</script>
+
+<style lang="scss">
+#app {
+
+  .card {
+    width: 200px;
+    height: 20px;
+    margin-bottom: 5px;
+  }
+
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false // 关闭保存时的 ESLint 检查
 })
